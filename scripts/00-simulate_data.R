db6cbd6e9c62bacdc43a7a3fa807ea4fc51e149f
Navya Hooda <navya.hooda@mail.utoronto.ca>
2024-01-24 07:25:26 +0000

    Added simulate data information

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -5,26 +5,60 @@
 # Contact: navya.hooda@mail.utoronto.ca 
 # License: MIT
 # Pre-requisites: 
-# Any other information needed? 
 
 
 #### Workspace setup ####
 library(tidyverse)
 install.packages("janitor")
 
-#### Simulate data ####
-simulated_data <-
-  tibble(
-    # Use 1 through to 25 to represent each division
-    "Division" = 1:25,
-    # Randomly pick an option, with replacement, 151 times
-    "Party" = sample(
-      x = c("Liberal", "Labor", "National", "Green", "Other"),
-      size = 25,
-      replace = TRUE
-    )
-  )
 
+# Defining columns from data to simulate
+
+# Ward Name 
+ward_name = c("Toronto Core", "Etobicoke North", "Etobicoke South", "Parkdale-High Park")
+
+
+# Population
+population = c(1:2761285)
+
+
+# Employment Status
+E_status = c("Self Employed", "Employed", "Unemployed", "Retired")
+
+# Income
+Income = c(1:400000)
+
+## Creating simulated data 
+
+set.seed(311) 
+
+#### Simulate data ####
+simulated_data <- tibble(population = sample(x = population, size =4, replace =  TRUE),
+                         ward_name = sample(x = ward_name, size = 4, replace = TRUE),
+                         Income = sample(x = Income, size = 4, replace = TRUE), 
+                         E_status = sample(x = E_status, size = 4, replace = TRUE))
+
+# show simulated data 
 simulated_data
 
+# Summaries of ward statistics by population 
 
+ward_by_population <- 
+  simulated_data %>%
+  group_by(population) %>%
+  summarise(
+    total_population = sum(population),
+    average_population = mean(population),
+    max_population = max(population),
+    min_population = min(population)
+  )
+
+
+#Bar plot for wards by population 
+ggplot(ward_by_population, aes(x = ward_name, y = population)) +
+  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+  labs(title = "ward by population",
+       x = "Ward Name",
+       y = "Population") +
+  theme_minimal()
+ward_by_population
