d7811cb5c53649ae8b2d07afe2c0376de81473ad
Navya Hooda <navya.hooda@mail.utoronto.ca>
2024-01-25 10:12:04 +0000

    Intial Edits on qmd, added sketches, and test script

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,47 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests cleaned data and data validation
+# Author: Navya Hooda
+# Date: 19 January 2024
+# Contact: navya.hooda@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
-library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Test data ####
+library(tidyverse)
+library(dplyr)
+
+# Testing reference from https://dplyr.tidyverse.org/reference/n_distinct.html 
+
+#### Test the data ####
+
+# data from 00-simulate_data to test
+
+source("scripts/00-simulate_data.R")
+source("scripts/02-data_cleaning.R")
+
+
+# check there are 25 rows one for each ward
+total_rows = nrow(transposed_data)
+total_rows == 25
+
+# check there are 6 cols 
+total_cols = ncol(transposed_data)
+total_cols == 7
+
+
+# check ward name is unique 
+ward_is_unique <- n_distinct(summarized_ward_data$ward_name)
+print(num_unique)
+ward_is_unique |> max() == 25
+
+# check in the simulated that there are 4 columns
+num_col = ncol(simulated_data)
+num_col == 4
+
+
+# check the income in simulated data follows max/min restrictions
+simulated_data$Income |> max() < 400000
+simulated_data$Income |> min() >= 1
+
+print(simulated_data$Income)
